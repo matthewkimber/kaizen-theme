bb4410f3c6b5a77145e9aaf60d1ccede154843ba
Matthew Kimber <matthew.kimber@gmail.com>
2010-09-05 22:45:48 -0600

    Almost there. Got most of the layout finished and a lot of the images done. Just need a bit more work on some standard components and it will be all good.

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function()
+{
+    $("#aspnetForm").wrap("<div id=\"container\" />");
+    $("#FooterDiv").appendTo($("body"));
+    $("#FooterDiv").wrap("<div id=\"footer\" />");
+});
