d0555894209bec05c83a49a658ee14f9cb95082b
Matthew Kimber <matthew.kimber@gmail.com>
2010-09-09 17:00:29 -0600

    More unfuddling the unfuddle plugin layout.

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ $(document).ready(function()
     $("#aspnetForm").wrap("<div id=\"container\" />");
     $("#FooterDiv").appendTo($("body"));
     $("#FooterDiv").wrap("<div id=\"footer\" />");
+    $("#UnfuddleTicketsDiv tr td:nth-child(4)").css("text-align", "right");
+    $("#UnfuddleTicketsDiv tr td:nth-child(4)").css("font-style", "italic");
+    $("#UnfuddleTicketsDiv tr td:nth-child(5)").css("display", "none");
+    $("#UnfuddleTicketsDiv tr td:nth-child(3)").css("display", "none");
+    $("#UnfuddleTicketsDiv tr td:nth-child(1)").css("display", "none");
+    $("#UnfuddleTicketsDiv tr").removeClass("priority_{priority}");
+    $("#UnfuddleTicketsDiv tr").removeClass("tablerow");
+    $("#UnfuddleTicketsDiv td").removeClass("priority_3");
+    $("#UnfuddleTicketsDiv td").addClass("TicketCell");
+    $("#UnfuddleTicketsDiv tr").addClass("TicketRow");
+    $("#UnfuddleTicketsDiv tr:nth-child(odd)").addClass("TicketAlternatingRow");
 });
