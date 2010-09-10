32ee627eff14382345e6315b2345d1722003ac91
Matthew Kimber <matthew.kimber@gmail.com>
2010-09-10 11:02:58 -0600

    Fixed the second showing column in the Unfuddle widget. For some reason I had the priority and not the status showing.

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ $(document).ready(function()
     $("#aspnetForm").wrap("<div id=\"container\" />");
     $("#FooterDiv").appendTo($("body"));
     $("#FooterDiv").wrap("<div id=\"footer\" />");
-    $("#UnfuddleTicketsDiv tr td:nth-child(4)").css("text-align", "right");
-    $("#UnfuddleTicketsDiv tr td:nth-child(4)").css("font-style", "italic");
+    $("#UnfuddleTicketsDiv tr td:nth-child(3)").css("text-align", "right");
+    $("#UnfuddleTicketsDiv tr td:nth-child(3)").css("font-style", "italic");
     $("#UnfuddleTicketsDiv tr td:nth-child(5)").css("display", "none");
-    $("#UnfuddleTicketsDiv tr td:nth-child(3)").css("display", "none");
+    $("#UnfuddleTicketsDiv tr td:nth-child(4)").css("display", "none");
     $("#UnfuddleTicketsDiv tr td:nth-child(1)").css("display", "none");
     $("#UnfuddleTicketsDiv tr").removeClass("priority_{priority}");
     $("#UnfuddleTicketsDiv tr").removeClass("tablerow");
